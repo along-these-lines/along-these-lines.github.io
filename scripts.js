3d0e8cee0b6e18cad38de6e1ca6dc3c315f3a05e
Emily Bowe <emilyrbowe@gmail.com>
2020-06-11 20:43:01 -0500

    More testing...

--- scripts.js
+++ scripts.js
@@ -5,13 +5,13 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
 
-    titleText = document.getElementById("lines");
+    var titleText = document.getElementById("lines");
     titleText.style.color = '#EE9D00';
     titleText.classList.add("fade");
 
   } else {
     document.getElementById("lines").style.color = "white";
-    titleText = document.getElementById("lines");
+    var titleText = document.getElementById("lines");
     titleText.classList.remove("fade");
   }
 }
