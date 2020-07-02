d93bf838c2228a97ee22364494a82cea470b90c0
Emily Bowe <emilyrbowe@gmail.com>
2020-07-02 13:35:12 -0500

    Fixed sticky header & changed logo styling

--- scripts.js
+++ scripts.js
@@ -8,12 +8,15 @@ function scrollFunction() {
   if (window.scrollTop > 70 || document.documentElement.scrollTop > 70) {
 
     var titleText = document.getElementById("lines");
-    titleText.style.color = '#EE9D00';
+    titleText.style.color = "#32F5F1";
+    // titleText.style.textShadow = "0 0 5px goldenrod" 
+    // titleText.style.color = '#EE9D00';
     titleText.classList.add("fade");
 
   } else {
     document.getElementById("lines").style.color = "white";
     var titleText = document.getElementById("lines");
     titleText.classList.remove("fade");
+    titleText.style.textShadow = "0 0 5px black"
   }
 }
